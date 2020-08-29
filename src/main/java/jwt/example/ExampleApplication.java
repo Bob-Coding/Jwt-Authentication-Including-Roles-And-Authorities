3028b26367aa1ad9c0f67338dfec55099d3f5156
Bob-Coding <bobgunsing@gmail.com>
2020-08-29 22:45:56 +0200

    moved tokensecret to app.properties

--- src/main/java/jwt/example/ExampleApplication.java
+++ src/main/java/jwt/example/ExampleApplication.java
@@ -1,5 +1,6 @@
 package jwt.example;
 
+import jwt.example.security.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -21,5 +22,10 @@ public class ExampleApplication {
     @Bean
     public ExampleApplicationContext exampleApplicationContext() {
         return new ExampleApplicationContext();
+    }
+
+    @Bean(name = "AppProperties")
+    public AppProperties getAppProperties() {
+        return new AppProperties();
     }
 }
--- src/main/java/jwt/example/security/SecurityConstants.java
+++ src/main/java/jwt/example/security/SecurityConstants.java
@@ -1,10 +1,16 @@
 package jwt.example.security;
 
+import jwt.example.ExampleApplicationContext;
+
 public class SecurityConstants {
     public static final long EXPIRATION_TIME = 864000000;  //10days
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/api/users";
-    public static final String TOKEN_SECRET = "dkjfofegdfssfsdfggtrjincwci";
 
+    //moved the TOKEN constant to application.properties
+    public static String getTokenSecret() {
+        AppProperties appProperties = (AppProperties) ExampleApplicationContext.getBean("AppProperties");  //method to acces components that were created by springframework
+        return appProperties.getTokenSecret();
+    }
 }
