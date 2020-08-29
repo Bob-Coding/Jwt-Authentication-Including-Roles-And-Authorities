7654864b818dc49ebd7d3707b3a638a26b802797
Bob-Coding <bobgunsing@gmail.com>
2020-08-29 20:05:30 +0200

    added websecurityconfig for signup endpoint to be public

--- src/main/java/jwt/example/controller/UserService.java
+++ src/main/java/jwt/example/controller/UserService.java
@@ -5,6 +5,7 @@ import jwt.example.model.UserEntity;
 import jwt.example.userDto.Utils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -54,6 +55,11 @@ public class UserService implements UserServiceInterface{
         UserDto returnValue = new UserDto();
         BeanUtils.copyProperties(storedUserDetails, returnValue);
         return returnValue;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null;
     }
 
     public String deleteUserById(long id) {
--- src/main/java/jwt/example/controller/UserServiceInterface.java
+++ src/main/java/jwt/example/controller/UserServiceInterface.java
@@ -1,8 +1,9 @@
 package jwt.example.controller;
 
 import jwt.example.userDto.UserDto;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserServiceInterface {
+public interface UserServiceInterface extends UserDetailsService {
     UserDto createUser(UserDto user);
     UserDto getUserByUserId(String userId);
 }
